8ea4b733bd33a8534d016aa59789949fcfa6cce4
Viacheslav Kuldyaev <slavalion1@gmail.com>
2021-09-23 01:17:12 +0300

    Second lesson commit

--- js/main.js
+++ js/main.js
@@ -1,34 +1,79 @@
-const products = [
-    {id: 1, title: 'Notebook', price: 2000, img: '1.jpg'},
-    {id: 2, title: 'Mouse', price: 20, img: "2.jpg" },
-    {id: 3, title: 'Keyboard', price: 200, img: "3.jpg"},
-    {id: 4, title: 'Gamepad', price: 50, img: "4.jpg"},
-];
-//Функция для формирования верстки каждого товара
-//Добавить в выводе изображение
-const renderProduct = (title, price, img) => {
-    return `<div class="product-item">
+class ProductList{
+    constructor(container='.products'){
+        this.container = container;
+        this.goods = [];
+        this._fetchProducts();
+    }
+    _fetchProducts(){
+        this.goods = [
+            {id: 1, title: 'Notebook', price: 2000, img: '1.jpg'},
+            {id: 2, title: 'Mouse', price: 20, img: "2.jpg" },
+            {id: 3, title: 'Keyboard', price: 200, img: "3.jpg"},
+            {id: 4, title: 'Gamepad', price: 50, img: "4.jpg"},
+        ];
+    }
+    render(){
+        const block = document.querySelector(this.container);
+        for(let product of this.goods){
+            const item = new ProductItem(product);
+             block.insertAdjacentHTML("beforeend",item.render());
+        }
+    }
+    getSum(){
+        let sumOfPrices = 0;
+        this.goods.forEach(item => {sumOfPrices += item.price});
+        alert(sumOfPrices);
+    }
+}
+
+class ProductItem{
+    constructor(product){
+        this.title = product.title;
+        this.id = product.id;
+        this.price = product.price;
+        this.img = product.img;
+    }
+    render(){
+           return `<div class="product-item">
                 <div class="image-place">
-                    <img class="product-img" src="img/${img}" alt="product icon" id=${img}/>
+                    <img class="product-img" src="img/${this.img}" alt="product icon" id=${this.img}/>
                 </div>
                 <div class="info-place">
                     <div class="namePrice">
-                        <h3>${title}</h3>
-                        <p>${price} руб.</p>
+                        <h3>${this.title}</h3>
+                        <p>${this.price} руб.</p>
                     </div>
                     <div class="buyBtn">
                         <button class="buy-btn">Купить</button>
                     </div>
                 </div>
             </div>`
-};
-const renderPage = list => {
-    const productsList = list.map(item => {
-                let newItem = document.createElement('div');
-                newItem.className = "product-item"  ;
-                newItem.innerHTML = renderProduct(item.title,item.price,item.img);
-                document.querySelector('.products').appendChild(newItem);
-    });
-};
+    }
+}
 
-renderPage(products);
+let list = new ProductList();
+list.render();
+list.getSum();
+
+class Cart {
+    addItem(){
+
+    }
+    removeItem(){
+
+    }
+    changeItem(){
+
+    }
+    render(){
+
+    }
+}
+
+class CartItem{
+    render(){
+
+    }
+}
+
+
